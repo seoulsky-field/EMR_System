6b6ded4ab425940a73c3faf61be102f5f7539f42
seoulsky-field <yeon2002.kj@dankook.ac.kr>
2021-11-20 16:29:19 +0900

    basic gui process code

--- EMR_main.py
+++ EMR_main.py
@@ -1,3 +1,37 @@
-print("test")
-print("Hello")
-print("test again")
+import sys
+import os
+import pydicom
+from PySide2 import QtUiTools, QtGui
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+
+class MainView(QMainWindow):    
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        global UI_set
+
+        UI_set = QtUiTools.QUiLoader().load(resource_path("EMR_GUI.ui"))
+        self.setCentralWidget(UI_set)
+        self.setWindowTitle("EMR System : v0.1")
+        self.resize(500,400)
+        self.show()
+
+
+#파일 경로
+#pyinstaller로 원파일로 압축할때 경로 필요함
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainView()
+
+    #main.show()
+    sys.exit(app.exec_())

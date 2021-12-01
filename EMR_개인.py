30eddc51cf6a42b75e7fe6f89485928702d7ccec
hyerim1127 <93864238+hyerim1127@users.noreply.github.com>
2021-12-02 01:38:26 +0900

    Add files via upload

--- EMR_개인.py
+++ EMR_개인.py
@@ -14,11 +14,11 @@ class MainView(QMainWindow):
         global UI_set
 
         # 환자 데이터 경로 지정 및 데이터 읽기
-        DATA_PATH = "D:\장혜림\Desktop\장혜림\pythonProject\emr.csv"
+        DATA_PATH = "D:/장혜림/Desktop/장혜림/pythonProject/patientData.csv"
         self.patientData = pd.read_csv(DATA_PATH)
 
         # GUI 로드
-        UI_set = QtUiTools.QUiLoader().load(resource_path("D:\장혜림\Desktop\장혜림\pythonProject\emr.ui"))
+        UI_set = QtUiTools.QUiLoader().load(resource_path("D:/장혜림/Desktop/장혜림/pythonProject/emr.ui"))
 
         self.setCentralWidget(UI_set)
         self.setWindowTitle("EMR System")
@@ -29,40 +29,59 @@ class MainView(QMainWindow):
         UI_set.BTN_del.clicked.connect(self.nameDiscordance)
 
     def nameCorrespond(self):
-        # 환자ID, 진료날짜, 의료 영상 열들을 target으로 하고, 의료 영상이 공백인 경우 모두 제거
-        target_columns = ['환자ID', '환자명', '진료 날짜', '주민번호', '성별', '주소', '혈액형', '키', '몸무게']
+        target_columns = ['환자ID', '환자명', '주민번호']
         needData = self.patientData[target_columns]
 
         global a
-        a = UI_set.LE_name.text()                        #환자 아이디 입력 받기
-        patientID = target_columns.loc[ ["환자ID"] : ]    #csv파일의 컬럼 중 "환자ID" 부분을 변수로 지정
+        a = UI_set.LE_name.text()
 
-        if a in patientID:                               #만약 입력 받은 아이디가 patientID에 있다면
-            UI_set.L_name.setText(a.needData(self))      #밑의 라벨에 a의 데이터 출력
-        else:
-            UI_set.L_name.setText(a + "ID의 환자가 존재하지 않습니다.")
+        id_list = []
+
+        personData = needData['환자명'].str.contains(a)
+        savePersonData = needData[personData]
+        print(savePersonData)
+
+        for a in range(0, len(personData)):
+
+            if a in personData and a not in id_list:
+                id_list.append(a)
+                UI_set.L_name.setText(str(a)+" 환자가 존재합니다.")     #str(a)로 하지 않으면 에러 발생, str(a)로 하면 숫자로 출력됨.
+                comboboxPersonData = "/".join(savePersonData)       #"환자ID/환자명/주민번호"가 14개 출력됨. 14개는 환자데이터 개수인듯
+                UI_set.patientCombo.addItem(comboboxPersonData)
+            elif a in personData and a in id_list:
+                continue
+            else:
+                UI_set.L_name.setText(a + " 환자가 존재하지 않습니다.")
 
     def nameDiscordance(self):
-
-        target_columns = ['환자ID', '환자명', '진료 날짜', '주민번호', '성별', '주소', '혈액형', '키', '몸무게']
+        target_columns = ['환자ID', '환자명', '주민번호']
         needData = self.patientData[target_columns]
 
-        # a = UI_set.LE_name.text()                           #환자 아이디 입력 받기
-        patientID = target_columns.loc[["환자ID"]:]          #csv파일의 컬럼 중 "환자ID" 부분을 변수로 지정
+        a = UI_set.LE_name.text()
 
-        if a in patientID:                                  #만약 입력받은 아이디가 patientID에 있으면
-            UI_set.L_name.setText(a.target_columns.loc[["환자명"]:] + "님을 삭제하시겠습니까?")
-                                                            #a의 데이터 중 환자명 추출+삭제할 것이냐고 묻기
-            UI_set.patientDelete(self)
-        else:
-            UI_set.L_name.setText("해당 환자가 존재하지 않습니다.")
+        id_list = []
+
+        personData = needData['환자명'].str.contains(a)
+        savePersonData = needData[personData]
+        print(savePersonData)
+
+        for a in range(0, len(personData)):
+
+            if a in personData and a not in id_list:
+                id_list.append(a)
+                UI_set.L_name.setText(str(a) + " 환자를 삭제합니다.")
+                self.patientDelete
+            elif a in personData and a in id_list:
+                continue
+            else:
+                UI_set.L_name.setText(a + " 환자가 존재하지 않습니다.")
 
     def patientDelete(self):
-        target_columns = ['환자ID', '환자명', '진료 날짜', '주민번호', '성별', '주소', '혈액형', '키', '몸무게']
+        target_columns = ['환자ID', '환자명', '주민번호']
         needData = self.patientData[target_columns]
 
-        dataframe = target_columns[target_columns["환자ID"] != a]     #환자ID가 a(입력받은환자아이디, 전역변수)가 아닌 리스트만 dataframe에 저장
-        print(dataframe)
+        dataframe = target_columns[target_columns["환자명"] != a]
+        UI_set.patientCombo.addItem(dataframe)
 
 
 #파일 경로
